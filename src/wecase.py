f1829c53e26c85e01aae68bca339066bd3909b15
biergaizi <biergaizi2009@gmail.com>
2013-02-09 23:28:40 +0800

    按照 PEP8 修正了代码格式，添加版权信息

--- src/wecase.py
+++ src/wecase.py
@@ -1,26 +1,30 @@
-#!/usr/bin/env pythoni
-# coding=utf-8
+#!/usr/bin/env python2
+# -*- coding: UTF-8 -*-
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
 # WeCase -- Linux Sina Weibo Client
 # Since 4th,Feb,2013
 # This is a TEST version
 # Wait for ...
+# Copyright: GPL v3 or later.
 
 # Well, Let's go!
 
 
-from PyQt4 import QtCore,QtGui
-from ui.MainWindow import Ui_frm_MainWindow
 import sys
+from PyQt4 import QtCore, QtGui
+from ui.MainWindow import Ui_frm_MainWindow
+
 
 class WeCaseWindow(QtGui.QMainWindow):
-    def __init__(self,parent=None):
-        QtGui.QWidget.__init__(self,parent)
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
         self.ui = Ui_frm_MainWindow()
         self.ui.setupUi(self)
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
-    wecasewin=WeCaseWindow()
+    wecasewin = WeCaseWindow()
     wecasewin.show()
     sys.exit(app.exec_())
+
