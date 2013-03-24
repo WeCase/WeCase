0ce2d443517a3dd51b69a247e555ff0140b50205
Tom Li <biergaizi2009@gmail.com>
2013-03-24 14:40:14 +0800

    修复所有 PEP8 警告

--- src/wecase.py
+++ src/wecase.py
@@ -14,7 +14,9 @@ import sys
 import os
 from datetime import datetime
 from dateutil import parser as time_parser
-import urllib.request, urllib.parse, urllib.error
+import urllib.request
+import urllib.parse
+import urllib.error
 import http.client
 import shelve
 import notify2 as pynotify
@@ -31,17 +33,17 @@ from AboutWindow_ui import Ui_About_Dialog
 APP_KEY = "1011524190"
 APP_SECRET = "1898b3f668368b9f4a6f7ac8ed4a918f"
 CALLBACK_URL = 'https://api.weibo.com/oauth2/default.html'
-OAUTH2_PARAMETER = {'client_id':       APP_KEY,
-                    'response_type':   'code',
-                    'redirect_uri':    CALLBACK_URL,
-                    'action':          'submit',
-                    'userId':          '',  # username
-                    'passwd':          '',  # password
-                    'isLoginSina':     0,
-                    'from':            '',
-                    'regCallback':     '',
-                    'state':           '',
-                    'ticket':          '',
+OAUTH2_PARAMETER = {'client_id': APP_KEY,
+                    'response_type': 'code',
+                    'redirect_uri': CALLBACK_URL,
+                    'action': 'submit',
+                    'userId': '',  # username
+                    'passwd': '',  # password
+                    'isLoginSina': 0,
+                    'from': '',
+                    'regCallback': '',
+                    'state': '',
+                    'ticket': '',
                     'withOfficalFlag': 0}
 config_path = os.environ['HOME'] + '/.config/wecase/config_db'
 cache_path = os.environ['HOME'] + '/.cache/wecase/'
@@ -75,19 +77,22 @@ class TweetModel(QtCore.QAbstractListModel):
 
 
 class TweetItem(QtCore.QAbstractItemModel):
-    typeRole            = QtCore.Qt.UserRole +  1
-    idRole              = QtCore.Qt.UserRole +  2
-    authorRole          = QtCore.Qt.UserRole +  3
-    avatarRole          = QtCore.Qt.UserRole +  4
-    contentRole         = QtCore.Qt.UserRole +  5
-    timeRole            = QtCore.Qt.UserRole +  6
-    originalIdRole      = QtCore.Qt.UserRole +  7
-    originalContentRole = QtCore.Qt.UserRole +  8
-    originalAuthorRole  = QtCore.Qt.UserRole +  9
-    originalTimeRole    = QtCore.Qt.UserRole + 10
-    thumbnailPicRole    = QtCore.Qt.UserRole + 11
+    typeRole = QtCore.Qt.UserRole + 1
+    idRole = QtCore.Qt.UserRole + 2
+    authorRole = QtCore.Qt.UserRole + 3
+    avatarRole = QtCore.Qt.UserRole + 4
+    contentRole = QtCore.Qt.UserRole + 5
+    timeRole = QtCore.Qt.UserRole + 6
+    originalIdRole = QtCore.Qt.UserRole + 7
+    originalContentRole = QtCore.Qt.UserRole + 8
+    originalAuthorRole = QtCore.Qt.UserRole + 9
+    originalTimeRole = QtCore.Qt.UserRole + 10
+    thumbnailPicRole = QtCore.Qt.UserRole + 11
 
-    def __init__(self, type="", id="", author="", avatar="", content="", time="", original_id="", original_content="", original_author="", original_time="", thumbnail_pic="", parent=None):
+    def __init__(self, type="", id="", author="", avatar="", content="",
+                 time="", original_id="", original_content="",
+                 original_author="", original_time="", thumbnail_pic="",
+                 parent=None):
         QtCore.QAbstractItemModel.__init__(self, parent)
 
         self.type = type
@@ -180,7 +185,9 @@ class LoginWindow(QtGui.QDialog, Ui_frm_Login):
     def setupSignals(self):
         self.pushButton_log.clicked.connect(self.login)
         self.chk_AutoLogin.clicked.connect(self.auto_login_clicked)
-        QtCore.QObject.connect(self.cmb_Users, QtCore.SIGNAL("currentIndexChanged(QString)"), self.setPassword)
+        QtCore.QObject.connect(self.cmb_Users,
+                               QtCore.SIGNAL("currentIndexChanged(QString)"),
+                               self.setPassword)
 
     def setupMyUi(self):
         self.txt_Password.setEchoMode(QtGui.QLineEdit.Password)
@@ -264,7 +271,7 @@ class LoginWindow(QtGui.QDialog, Ui_frm_Login):
 
         conn = http.client.HTTPSConnection('api.weibo.com')
         conn.request('POST', '/oauth2/authorize', postdata,
-                     {'Referer':       authorize_url,
+                     {'Referer': authorize_url,
                       'Content-Type': 'application/x-www-form-urlencoded'})
 
         res = conn.getresponse()
@@ -303,13 +310,14 @@ class WeCaseWindow(QtGui.QMainWindow, Ui_frm_MainWindow):
     def __init__(self, parent=None):
         QtGui.QMainWindow.__init__(self, parent)
         self.setupUi(self)
-        self.tweetViews = [self.homeView, self.mentionsView, self.commentsView, self.myView]
+        self.tweetViews = [self.homeView, self.mentionsView, self.commentsView,
+                           self.myView]
         self.setupModels()
         self.setupMyUi()
         self.setupSignals()
         self.IMG_AVATAR = -2
         self.IMG_THUMB = -1
-        self.TIMER_INTERVAL = 30  # TODO:30 Seconds by default, can be modify with settings window
+        self.TIMER_INTERVAL = 30  # TODO: Can be modify with settings window
         self.notify = Notify()
         self.timer = WTimer(self.TIMER_INTERVAL, self.show_notify)
         self.timer.start()
@@ -317,7 +325,8 @@ class WeCaseWindow(QtGui.QMainWindow, Ui_frm_MainWindow):
     def setupMyUi(self):
         for tweetView in self.tweetViews:
             tweetView.setResizeMode(tweetView.SizeRootObjectToView)
-            tweetView.setSource(QtCore.QUrl.fromLocalFile(myself_path + "/ui/TweetList.qml"))
+            tweetView.setSource(
+                QtCore.QUrl.fromLocalFile(myself_path + "/ui/TweetList.qml"))
             tweetView.rootContext().setContextProperty("mainWindow", self)
 
     def setupSignals(self):
@@ -350,26 +359,20 @@ class WeCaseWindow(QtGui.QMainWindow, Ui_frm_MainWindow):
 
     def setupModels(self):
         self.all_timeline = TweetModel(TweetItem(), self)
-        self.homeView.rootContext().setContextProperty("mymodel", self.all_timeline)
+        self.homeView.rootContext().setContextProperty("mymodel",
+                                                       self.all_timeline)
         self.mentions = TweetModel(TweetItem(), self)
-        self.mentionsView.rootContext().setContextProperty("mymodel", self.mentions)
+        self.mentionsView.rootContext().setContextProperty("mymodel",
+                                                           self.mentions)
         self.comment_to_me = TweetModel(TweetItem(), self)
-        self.commentsView.rootContext().setContextProperty("mymodel", self.comment_to_me)
+        self.commentsView.rootContext().setContextProperty("mymodel",
+                                                           self.comment_to_me)
         self.my_timeline = TweetModel(TweetItem(), self)
-        self.myView.rootContext().setContextProperty("mymodel", self.my_timeline)
+        self.myView.rootContext().setContextProperty("mymodel",
+                                                     self.my_timeline)
 
     def get_timeline(self, timeline, model, more=False):
-        def prefetchImage(url, img_type=-2):
-            # disabled. QML can fetch images by itself
-            filename = url.split("/")[img_type]
-
-            if not os.path.exists(cache_path + filename):
-                urllib.request.urlretrieve(url, cache_path + filename)
-            return
-
         for count, item in enumerate(timeline):
-            #prefetchImage(item['user']['profile_image_url'], self.IMG_AVATAR)
-
             # tweet (default), comment or retweet?
             item_type = "tweet"
 
@@ -402,25 +405,36 @@ class WeCaseWindow(QtGui.QMainWindow, Ui_frm_MainWindow):
             # thumb pic
             try:
                 item_thumb_pic = None
-                #prefetchImage(item['thumbnail_pic'], self.IMG_THUMB)
                 item_thumb_pic = item['thumbnail_pic']
             except KeyError:
                 try:
-                    #prefetchImage(item['retweeted_status']['thumbnail_pic'], self.IMG_THUMB)
                     item_thumb_pic = item['retweeted_status']['thumbnail_pic']
                 except KeyError:
                     pass
 
             # tweet
-            tweet = TweetItem(type=item_type, id=item_id, author=item_author, avatar=item_author_avatar, content=item_content, time=item_content_time)
+            tweet = TweetItem(type=item_type, id=item_id, author=item_author,
+                              avatar=item_author_avatar, content=item_content,
+                              time=item_content_time)
 
             if item_type == "comment":
                 # comment
-                tweet = TweetItem(type=item_type, id=item_id, author=item_author, avatar=item_author_avatar, content=item_content, time=item_content_time, original_id=item_comment_to_original_id)
+                tweet = TweetItem(type=item_type, id=item_id,
+                                  author=item_author,
+                                  avatar=item_author_avatar,
+                                  content=item_content, time=item_content_time,
+                                  original_id=item_comment_to_original_id)
 
             if item_type == "retweet":
                 # retweet
-                tweet = TweetItem(type=item_type, id=item_id, author=item_author, avatar=item_author_avatar, content=item_content, time=item_content_time, original_id=item_original_id, original_content=item_original_content, original_author=item_original_author, original_time=item_original_time)
+                tweet = TweetItem(type=item_type, id=item_id,
+                                  author=item_author,
+                                  avatar=item_author_avatar,
+                                  content=item_content, time=item_content_time,
+                                  original_id=item_original_id,
+                                  original_content=item_original_content,
+                                  original_author=item_original_author,
+                                  original_time=item_original_time)
 
             if not item_thumb_pic is None:
                 # thumb pic
@@ -430,20 +444,27 @@ class WeCaseWindow(QtGui.QMainWindow, Ui_frm_MainWindow):
         self.signalLoadFinished.emit(more)
 
     def get_all_timeline(self, page=1, reset_remind=False, more=False):
-        all_timelines = self.client.statuses.home_timeline.get(page=page).statuses
-        threading.Thread(group=None, target=self.get_timeline, args=(all_timelines, self.all_timeline, more)).start()
+        all_timelines = self.client.statuses.home_timeline.get(
+            page=page).statuses
+        threading.Thread(group=None, target=self.get_timeline,
+                         args=(all_timelines, self.all_timeline, more)).start()
         self.all_timeline_page = page
         if reset_remind:
             self.tabWidget.setTabText(0, "Weibo")
 
     def get_my_timeline(self, page=1, reset_remind=False, more=False):
-        my_timelines = self.client.statuses.user_timeline.get(page=page).statuses
-        threading.Thread(group=None, target=self.get_timeline, args=(my_timelines, self.my_timeline, more)).start()
+        my_timelines = self.client.statuses.user_timeline.get(
+            page=page).statuses
+        threading.Thread(group=None, target=self.get_timeline,
+                         args=(my_timelines, self.my_timeline, more)).start()
         self.my_timeline_page = page
 
     def get_mentions_timeline(self, page=1, reset_remind=False, more=False):
-        mentions_timelines = self.client.statuses.mentions.get(page=page).statuses
-        threading.Thread(group=None, target=self.get_timeline, args=(mentions_timelines, self.mentions, more)).start()
+        mentions_timelines = self.client.statuses.mentions.get(
+            page=page).statuses
+        threading.Thread(group=None, target=self.get_timeline,
+                         args=(
+                             mentions_timelines, self.mentions, more)).start()
         self.mentions_page = page
         if reset_remind:
             self.client.remind.set_count.post(type="mention_status")
@@ -451,7 +472,8 @@ class WeCaseWindow(QtGui.QMainWindow, Ui_frm_MainWindow):
 
     def get_comment_to_me(self, page=1, reset_remind=False, more=False):
         comments_to_me = self.client.comments.to_me.get(page=page).comments
-        threading.Thread(group=None, target=self.get_timeline, args=(comments_to_me, self.comment_to_me, more)).start()
+        threading.Thread(group=None, target=self.get_timeline, args=(
+            comments_to_me, self.comment_to_me, more)).start()
         self.comment_to_me_page = page
         if reset_remind:
             self.client.remind.set_count.post(type="cmt")
@@ -491,7 +513,8 @@ class WeCaseWindow(QtGui.QMainWindow, Ui_frm_MainWindow):
 
         if reminds['mention_status'] != 0:
             msg += "%d unread @ME\n" % reminds['mention_status']
-            self.signalSetTabText.emit(1, "@Me(%d)" % reminds['mention_status'])
+            self.signalSetTabText.emit(1,
+                                       "@Me(%d)" % reminds['mention_status'])
             num_msg += 1
 
         if reminds['cmt'] != 0:
@@ -500,7 +523,9 @@ class WeCaseWindow(QtGui.QMainWindow, Ui_frm_MainWindow):
             num_msg += 1
 
         if num_msg != 0:
-            self.notify.showMessage("WeCase", msg, image="notification-message-email")  # TODO: image can use our images in rcc
+            #TODO: image can use our images in rcc
+            self.notify.showMessage("WeCase", msg,
+                                    image="notification-message-email")
 
     @QtCore.pyqtSlot(int, str)
     def setTabText(self, index, string):
@@ -560,16 +585,19 @@ class WeCaseWindow(QtGui.QMainWindow, Ui_frm_MainWindow):
 
     @QtCore.pyqtSlot(str, str)
     def reply(self, idstr, cidstr):
-        wecase_new = NewpostWindow(action="reply", id=int(idstr), cid=int(cidstr))
+        wecase_new = NewpostWindow(action="reply", id=int(idstr),
+                                   cid=int(cidstr))
         wecase_new.client = self.client
         wecase_new.exec_()
 
     @QtCore.pyqtSlot(str, str)
     def look_orignal_pic(self, thumbnail_pic, tweetid):
-        threading.Thread(group=None, target=self.fetch_open_original_pic, args=(thumbnail_pic, tweetid)).start()
+        threading.Thread(group=None, target=self.fetch_open_original_pic,
+                         args=(thumbnail_pic, tweetid)).start()
 
     def fetch_open_original_pic(self, thumbnail_pic, tweetid):
-        original_pic = thumbnail_pic.replace("thumbnail", "large")  # A simple trick ... ^_^
+        original_pic = thumbnail_pic.replace("thumbnail",
+                                             "large")  # A simple trick ... ^_^
         extname = original_pic.split("/")[-1].split(".")[-1]
         localfile = cache_path + "Preview." + extname
         urllib.request.urlretrieve(original_pic, localfile)
@@ -585,15 +613,19 @@ class WeCaseWindow(QtGui.QMainWindow, Ui_frm_MainWindow):
         get_timeline(page=1, reset_remind=True, more=True)
 
     def get_current_tweetView(self):
-        tweetViews = {0: self.homeView, 1: self.mentionsView, 2: self.commentsView, 3: self.myView}
+        tweetViews = {0: self.homeView, 1: self.mentionsView,
+                      2: self.commentsView, 3: self.myView}
         return tweetViews[self.tabWidget.currentIndex()]
 
     def get_current_model(self):
-        models = {0: self.all_timeline, 1: self.mentions, 2: self.comment_to_me, 3: self.my_timeline}
+        models = {0: self.all_timeline, 1: self.mentions,
+                  2: self.comment_to_me,
+                  3: self.my_timeline}
         return models[self.tabWidget.currentIndex()]
 
     def get_current_function(self):
-        functions = {0: self.get_all_timeline, 1: self.get_mentions_timeline, 2: self.get_comment_to_me, 3: self.get_my_timeline}
+        functions = {0: self.get_all_timeline, 1: self.get_mentions_timeline,
+                     2: self.get_comment_to_me, 3: self.get_my_timeline}
         return functions[self.tabWidget.currentIndex()]
 
 
@@ -665,7 +697,8 @@ class NewpostWindow(QtGui.QDialog, Ui_NewPostWindow):
     def reply(self):
         text = str(self.textEdit.toPlainText())
         try:
-            self.client.comments.reply.post(id=int(self.id), cid=int(self.cid), comment=text)
+            self.client.comments.reply.post(id=int(self.id), cid=int(self.cid),
+                                            comment=text)
             self.notify.showMessage("WeCase", "Reply Success!")
             self.signalSendSuccessful.emit()
         except APIError as e:
@@ -677,7 +710,8 @@ class NewpostWindow(QtGui.QDialog, Ui_NewPostWindow):
 
         try:
             if self.image:
-                self.client.statuses.upload.post(status=text, pic=open(self.image, "rb"))
+                self.client.statuses.upload.post(status=text,
+                                                 pic=open(self.image, "rb"))
             else:
                 self.client.statuses.update.post(status=text)
 
@@ -690,11 +724,14 @@ class NewpostWindow(QtGui.QDialog, Ui_NewPostWindow):
         self.image = None
 
     def add_image(self):
+        ACCEPT_TYPE = "Images (*.png *.jpg *.bmp *.gif)"
         if self.image:
             self.image = None
             self.pushButton_picture.setText("Picture")
         else:
-            self.image = QtGui.QFileDialog.getOpenFileName(self, "Choose a image", filter="Images (*.png *.jpg *.bmp *.gif)")
+            self.image = QtGui.QFileDialog.getOpenFileName(self,
+                                                           "Choose a image",
+                                                           filter=ACCEPT_TYPE)
             self.pushButton_picture.setText("Remove the picture")
 
     @QtCore.pyqtSlot(str)
@@ -729,7 +766,8 @@ class Notify():
 
     def showMessage(self, title, text, image=""):
         self.n.update(title, text, image)
-        self.n.set_timeout(self.timeout * 1000)  # TODO: user should be able to adjust the time by settings window
+        # TODO: user should be able to adjust the time by settings window
+        self.n.set_timeout(self.timeout * 1000)
         self.n.show()
 
 
@@ -737,6 +775,7 @@ class AboutWindow(QtGui.QDialog, Ui_About_Dialog):
     def __init__(self, parent=None):
         QtGui.QDialog.__init__(self, parent)
         self.setupUi(self)
+
 
 if __name__ == "__main__":
     try:
