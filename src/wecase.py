9114ed7c4c699a227f3ceb60c293d8e9eb68fb0c
Tom Li <biergaizi2009@gmail.com>
2013-04-27 15:27:16 +0800

    再次修正字符串格式化

--- src/wecase.py
+++ src/wecase.py
@@ -371,13 +371,13 @@ class WeCaseWindow(QtGui.QMainWindow, Ui_frm_MainWindow):
                                      % reminds['status'])
 
         if reminds['mention_status'] and self.remindMentions:
-            msg += self.tr("%d unread @ME" % reminds['mention_status']) + "\n"
+            msg += self.tr("%d unread @ME") % reminds['mention_status'] + "\n"
             self.tabTextChanged.emit(1, self.tr("@Me(%d)")
                                      % reminds['mention_status'])
             num_msg += 1
 
         if reminds['cmt'] and self.remindComments:
-            msg += self.tr("%d unread comment(s)" % reminds['cmt']) + "\n"
+            msg += self.tr("%d unread comment(s)") % reminds['cmt'] + "\n"
             self.tabTextChanged.emit(2, self.tr("Comments(%d)")
                                      % reminds['cmt'])
             num_msg += 1
