0eb76921079537c04d689524aafd31eae62a9e1b
Tom Li <biergaizi2009@gmail.com>
2013-05-26 12:24:00 +0800

    异步图片容错性增强

--- src/WAsyncLabel.py
+++ src/WAsyncLabel.py
@@ -2,6 +2,7 @@ import os
 from time import sleep
 from threading import Thread
 import urllib.request
+import http.client
 from PyQt4 import QtCore, QtGui
 from const import cache_path as down_path
 
@@ -36,19 +37,26 @@ class WAsyncLabel(QtGui.QLabel):
                args=(self.url, filename)).start()
 
     def _fetch(self, url, filename):
-        while 1:
-            if os.path.exists(down_path + filename + ".down"):
-                sleep(0.5)
-                continue
-            else:
-                break
+        while os.path.exists(down_path + filename + ".down"):
+            sleep(0.5)
+            continue
 
         if os.path.exists(down_path + filename):
             return self.downloaded.emit(down_path + filename)
 
-        urllib.request.urlretrieve(url, down_path + filename + ".down")
-        os.rename(down_path + filename + ".down",
-                  down_path + filename)
+        while 1:
+            try:
+                urllib.request.urlretrieve(url, down_path + filename + ".down")
+                break
+            except http.client.BadStatusLine:
+                continue
+
+        try:
+            os.rename(down_path + filename + ".down",
+                      down_path + filename)
+        except FileNotFoundError:
+            pass
+
         return self.downloaded.emit(down_path + filename)
 
     def mouseReleaseEvent(self, e):
