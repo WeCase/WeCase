737fbe857f743fdd419d6e883f9dd5ee6cd56da7
Tom Li <biergaizi2009@gmail.com>
2013-06-21 12:17:04 +0800

    添加三个单元测试

--- src/WTimeParser.py
+++ src/WTimeParser.py
@@ -38,8 +38,9 @@ class WTimeParser():
     def parse(self, time_string):
         """
         Sina's time-string parser.
-        Input a timeString, like Sat Apr 06 00:49:30 +0800 2013
-        Output a datetime(2013, 4, 6, 0, 49, 30, tzinfo=tzoffset(None, 28800))
+
+        >>> t.parse("Sat Apr 06 00:49:30 +0800 2013")
+        datetime(2013, 4, 6, 0, 49, 30, tzinfo=tzoffset(None, 28800))
         """
 
         hour = minute = second = 0
@@ -60,3 +61,19 @@ class WTimeParser():
 
         return datetime(year, month, day, hour, minute, second,
                         tzinfo=tzoffset(None, timezone_offset))
+
+
+import unittest
+class WTimeParserTest(unittest.TestCase):
+
+    def setUp(self):
+        self.parser = WTimeParser()
+
+    def test_parse(self):
+        got_daytime = self.parser.parse("Sat Apr 06 00:49:30 +0800 2013")
+        except_daytime = datetime(2013, 4, 6, 0, 49, 30, tzinfo=tzoffset(None, 28800))
+        self.assertEqual(got_daytime, except_daytime)
+
+
+if __name__ == "__main__":
+    unittest.main()
