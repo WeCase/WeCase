23e94431c432e6c1cd22259981ffa79f4c9f4990
Tom Li <biergaizi2009@gmail.com>
2013-07-02 18:33:54 +0800

    完善依赖图生成脚本

--- src/depgraph.py
+++ src/depgraph.py
@@ -6,15 +6,23 @@ def process_import(filename, statement):
     statement = statement.replace(",", " ")
     modules = statement.split()
     for module in modules[1:]:
-        print("%s -> %s" % (filename, module))
+        print('"%s" -> "%s"' % (filename, module))
 
 def process_from(filename, statement):
     statement = statement.replace(",", " ")
     modules = statement.split()
     main_module = modules[1]
     for module in modules[3:]:
-        print("%s -> %s -> %s" % (filename, main_module, module))
+        print('"%s" -> "%s" -> "%s"' % (filename, main_module, module))
 
+def print_header():
+    print("digraph WeCase {")
+    print("ratio=2")
+
+def print_footer():
+    print("}")
+
+print_header()
 for line in sys.stdin:
     line = line.replace("\n", "")
     if line.endswith(".py"):
@@ -24,3 +32,4 @@ for line in sys.stdin:
             modulename = process_import(filename, line)
         elif line.startswith("from"):
             modulename = process_from(filename, line)
+print_footer()
