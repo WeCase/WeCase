7b4e7868d6730ee9352cf7311c2ad724684eb477
Tom Li <biergaizi2009@gmail.com>
2013-08-16 14:10:51 +0800

    Hasttag 为两井号之间任意内容，Fixed Issue #81

--- src/TweetListWidget.py
+++ src/TweetListWidget.py
@@ -613,7 +613,7 @@ class SingleTweetWidget(QtGui.QFrame):
         return new_text
 
     def _create_hashtag(self, text):
-        HASHTAG_RE = re.compile("([#]+[a-zA-Z0-9_\u4e00-\u9fa5\s]+[#])")
+        HASHTAG_RE = re.compile("(#.*?#)")
         regex = re.compile(HASHTAG_RE)
         new_text = regex.sub(r"""<a href='hashtag:///\1'>\1</a>""", text)
         return new_text
