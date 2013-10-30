0dc3839c4cba36cfb0040914fde8551779c168f4
Tom Li <biergaizi2009@gmail.com>
2013-10-30 16:43:41 +0800

    配置解释器和修正和增强

--- src/WConfigParser.py
+++ src/WConfigParser.py
@@ -8,16 +8,19 @@ class WConfigParser():
 
     ITEM = {
         "section": "", "type": None,
-        "name": "", "default": None
+        "name": "", "alias": "",
+        "default": None
     }
 
     def __init__(self, schema, config, section=""):
+        super(WConfigParser, self).__setattr__("loaded", False)
         self._section = section
         self._config_path = config
         self._options = []
         self._config = ConfigParser()
         self._config.read(config)
         self.__parse__(schema)
+        super(WConfigParser, self).__setattr__("loaded", True)
 
     def __parse__(self, schema):
         schema = open(schema)
@@ -30,7 +33,10 @@ class WConfigParser():
                     config_item = deepcopy(self.ITEM)
                 continue
 
-            name, value = line.replace("\n", "").replace(" ", "").split("=")
+            name, value = line.replace("\n", "").split("=")
+            name = name.strip()
+            value = value.strip()
+
             try:
                 config_item[name] = value
             except KeyError:
@@ -39,20 +45,37 @@ class WConfigParser():
 
     def _get_option(self, name):
         for i in self._options:
-            if i["name"] == name:
+            if i["name"] == name or i["alias"] == name:
                 return i
+
+    def __setattr__(self, attr, value):
+        if not self.loaded:
+            super(WConfigParser, self).__setattr__(attr, value)
+        else:
+            if not self._config.has_section(self._section):
+                self._config[self._section] = {}
+            self._config[self._section][attr] = str(value)
 
     def __getattr__(self, attr):
         option = self._get_option(attr)
         if not option:
-            raise AttributeError
+            raise AttributeError("WConfigParser object has no attribute '%s'" % attr)
 
+        type = eval(option["type"])
         try:
-            return eval("%s(%s)" % (option["type"], self._config[self._section][attr]))
-        except (KeyError, TypeError):
-            return eval("%s(%s)" % (option["type"], option["default"]))
+            value = self._config[self._section][attr]
+        except KeyError:
+            value = option["default"]
+
+        if type is str:
+            return value
+        else:
+            return type(eval(value))
 
     def save(self):
         # TODO: Create and check the lock file during writing
         with open(self._config_path, "w+") as config_file:
+            config_file.seek(0)
+            config_file.write("# WeCase Configuration File, by WeCase Project.\n")
+            config_file.write("# DO NOT EDIT ON NORMAL PURPOSE.\n\n")
             self._config.write(config_file)
