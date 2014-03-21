4e8a21229916aba6644a88f39a9c82b02e165d66
Tom Li <biergaizi2009@gmail.com>
2014-03-21 22:25:11 +0800

    Revert "仅使用 pid 防止重复运行可能会发生碰撞，因此还使用 argv[1]"
    This reverts commit 1a35d7be7bf695fd5ab6b3133ba594b5677282e1.
    
    Conflicts:
    	src/LoginInfo.py

--- src/LoginInfo.py
+++ src/LoginInfo.py
@@ -7,7 +7,6 @@
 # License: GPL v3 or later.
 
 
-from sys import argv
 import os
 import tempfile
 from WeHack import pid_running
@@ -33,11 +32,11 @@ class LoginInfo():
             f.seek(0)
             for line in f:
                 line = line[:-1]  # \n
-                account, pid, argv1 = line.split(" ")
-                if pid_running(int(pid)) and argv1 == argv[1]:
+                account, pid = line.split(" ")
+                if pid_running(int(pid)):
                     accounts.append(account)
         return accounts
 
     def add_account(self, account):
         with self._open() as f:
-            f.write("%s %d %s\n" % (account, os.getpid(), argv[1]))
+            f.write("%s %d\n" % (account, os.getpid()))
