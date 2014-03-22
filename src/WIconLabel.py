a25fac2fe632a01942027d52eeec2e7793d59119
Tom Li <biergaizi2009@gmail.com>
2014-03-22 10:14:17 +0800

    WIconWidget: a simpler reimplementation.

--- src/WIconLabel.py
+++ src/WIconLabel.py
@@ -9,53 +9,36 @@
 from PyQt4 import QtCore, QtGui
 
 
-class WIconLabel(QtGui.QLabel):
+class WIconLabel(QtGui.QWidget):
 
     clicked = QtCore.pyqtSignal()
-    imageAtLeft = 1
-    imageAtRight = 2
-    imageAtTop = 3
-    imageAtBottom = 4
 
     def __init__(self, parent=None):
-        super(WIconLabel, self).__init__(parent)
-        self._icon = ""
-        self._icon_code = ""
-        self._text = ""
-        self._position = self.imageAtLeft
-
-    def text(self):
-        return self._text
-
-    def setText(self, text):
-        self._text = text
-        if self._position != self.imageAtBottom:
-            text = "<center>" + self._icon_code + text + "</center>"
-        else:
-            text = "<center>" + text + self._icon_code + "</center>"
-        super(WIconLabel, self).setText(text)
-
-    def icon(self):
-        return self._icon
+        super().__init__(parent)
+        self._icon = QtGui.QPixmap()
+        self._text = None
 
     def setIcon(self, icon):
-        self._icon = icon
-        if self._position == self.imageAtLeft:
-            self._icon_code = "<img src=\"%s\" />" % icon
-        elif self._position == self.imageAtRight:
-            self._icon_code = "<img src=\"%s\" />" % icon
-        elif self._position == self.imageAtTop:
-            self._icon_code = "<img src=\"%s\" />" % icon + "<br />"
-        elif self._position == self.imageAtBottom:
-            self._icon_code = "<br />" + "<img src=\"%s\" />" % icon
-        self.setText(self._text)
+        self._icon.load(icon)
+        self.update()
 
-    def position(self):
-        return self._position
+    def setText(self, text):
+        if self._text is None:
+            self._text = QtGui.QStaticText()
+        self._text.setText(text)
+        self.update()
 
-    def setPosition(self, pos):
-        self._position = pos
-        self.setIcon(self._icon)
+    def paintEvent(self, e):
+        painter = QtGui.QPainter(self)
+        painter.drawPixmap(0, 0, self._icon)
+        if self._text:
+            painter.drawStaticText(self._icon.width(), 0, self._text)
+
+    def sizeHint(self):
+        if self._text:
+            return QtCore.QSize(self._icon.width() + self._text.size().width(), self._icon.height())
+        else:
+            return self._icon.size()
 
     def mouseReleaseEvent(self, e):
         self.clicked.emit()
