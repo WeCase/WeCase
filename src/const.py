fb258ff0eff907938e4adcdfbf97161a8b26f20c
Tom Li <biergaizi2009@gmail.com>
2014-07-23 05:41:51 +0800

    const: remove OAUTH2_PARAMETER since rpweibo

--- src/const.py
+++ src/const.py
@@ -9,16 +9,4 @@
 APP_KEY = "1011524190"
 APP_SECRET = "1898b3f668368b9f4a6f7ac8ed4a918f"
 CALLBACK_URL = 'https://api.weibo.com/oauth2/default.html'
-OAUTH2_PARAMETER = {'client_id': APP_KEY,
-                    'response_type': 'code',
-                    'redirect_uri': CALLBACK_URL,
-                    'action': 'submit',
-                    'userId': '',  # username
-                    'passwd': '',  # password
-                    'isLoginSina': 0,
-                    'from': '',
-                    'regCallback': '',
-                    'state': '',
-                    'ticket': '',
-                    'withOfficalFlag': 0}
 MAX_IMAGE_BYTES = 5 * 1024 * 1024  # 5 MiB
