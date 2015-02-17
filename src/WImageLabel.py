WeCase/WeCase

commit 31f325e33e3a2d881d2a038a0ff7e99222a2706e

WImageLabel: accept a file instead of a path
Accept a file instead of a path, But it also works with a path
for now, because of the hack in WMovie.

src/WImageLabel.py
@@ -15,21 +15,18 @@ class WImageLabel(QtGui.QLabel):
 
     def __init__(self, parent=None):
         super(WImageLabel, self).__init__(parent)
-        self._imagePath = None
+        self._imageFile = None
 
-    def setImageFile(self, path, width=-1, height=-1):
-        self._imagePath = path
+    def setImageFile(self, file, width=-1, height=-1):
+        self._imageFile = file
 
         size = QtCore.QSize(width, height)
         if size.isValid():
             self.sizeHint = lambda: size
 
     def start(self):
-        if self.movie():
-            self._imagePath = self.movie().fileName()
-        else:
-            movie = WMovie(self._imagePath)
-            self.setMovie(movie)
+        movie = WMovie(self._imageFile)
+        self.setMovie(movie)
         self.movie().start()
 
     def stop(self):

commit 296bd7cede392ffb73e8b72acb8205530dd7d055

WImageLabel: check if self.movie() is set
A image for WImageLabel can be set not only by calling
setImageFile, but also calling other methods from its parent.

Therefore, We also need to check if self.movie() is set. This is
a regression in 31f325e33e3a2d881d2a038a0ff7e99222a2706e.

src/WImageLabel.py
@@ -25,7 +25,12 @@ class WImageLabel(QtGui.QLabel):
             self.sizeHint = lambda: size
 
     def start(self):
-        movie = WMovie(self._imageFile)
+        if self.movie():
+            movie = self.movie()
+        else:
+            assert self._imageFile
+            movie = WMovie(self._imageFile)
+
         self.setMovie(movie)
         self.movie().start()
 