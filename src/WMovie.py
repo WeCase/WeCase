89294a9381653453707185adec65c8a3ce81f6a0
Tom Li <biergaizi2009@gmail.com>
2015-02-17 18:18:19 +0800

    WMovie: seek and close the file properly.
    First of all, we need to seek the file to the beginning, or we will
    get no data if it is not the first time we read it.
    
    Second, if user passes a path and we open a file, we need to close
    it after we use it.

--- src/WMovie.py
+++ src/WMovie.py
@@ -12,13 +12,21 @@ class WMovie(QtGui.QMovie):
 
     def __init__(self, file, parent=None):
         super().__init__(parent)
+
         # HACK: allow to use a path
+        need_close = False
         if isinstance(file, str):
+            # we only need to close files that were opened by us
+            need_close = True
             file = open(file, "r")
 
-        data = b""
+        file.seek(0)
         data = file.read()
-        file.close()
+
+        if need_close:
+            file.close()
+
+        self._fileName = file.name
         self._imageData = QtCore.QBuffer()
         self._imageData.setData(data)
         self.setDevice(self._imageData)
